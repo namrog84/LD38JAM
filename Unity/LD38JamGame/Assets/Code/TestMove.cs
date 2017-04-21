275563dfac1eefe5aa6f165bec4f2c65193f4f4f
Adam <namrog84@gmail.com>
2017-04-21 13:12:03 -0700

    Test Guy can move and beep

---
+++ Unity/LD38JamGame/Assets/Code/TestMove.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestMove : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    public float speed = 5.0f;
+	// Update is called once per frame
+	void Update () {
+        var pos = transform.position;
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            pos.y += speed * Time.deltaTime;
+            
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            pos.y -= speed * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            pos.x -= speed * Time.deltaTime;
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            pos.x += speed * Time.deltaTime;
+        }
+        transform.position = pos;
+
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            GetComponent<AudioSource>().Play();
+        }
+    }
+}
+
