b7092344adfee8b116b47a6f14d59a083683aaca
Adam <namrog84@gmail.com>
2017-04-23 13:53:44 -0700

    updating art

--- Unity/LD38JamGame/Assets/Code/AssetManager.cs
+++ Unity/LD38JamGame/Assets/Code/AssetManager.cs
@@ -11,14 +11,14 @@ public class AssetManager : MonoBehaviour {
     {
         SpriteMap = new Dictionary<int, Sprite>()
         {
-        {   TileType.Apartment, Resources.Load<Sprite>("Art/SimpleHouse")   },
-        {   TileType.WaterFarm, Resources.Load<Sprite>("Art/fishfarm")   },
+        {   TileType.Apartment, Resources.Load<Sprite>("Art/houseWater")   },
+        {   TileType.WaterFarm, Resources.Load<Sprite>("Art/fishfarm5")   },
         {   TileType.GrassFarm, Resources.Load<Sprite>("Art/grassfarm")   },
         {   TileType.SpacePort, Resources.Load<Sprite>("Art/LaunchStation")   },
         {   TileType.RecreationPark, Resources.Load<Sprite>("Art/Recreational")   },
         {   TileType.DirtEnergy, Resources.Load<Sprite>("Art/powerLand")   },
-        {   TileType.WaterConservation, Resources.Load<Sprite>("Art/WaterRecycle")   },
-        {   TileType.WaterEnergy, Resources.Load<Sprite>("Art/powerWater")   },
+        {   TileType.WaterConservation, Resources.Load<Sprite>("Art/RecycleWater")   },
+        {   TileType.WaterEnergy, Resources.Load<Sprite>("Art/powerWater2")   },
         {   TileType.Water, Resources.Load<Sprite>("Art/testWater")   },
         {   TileType.Dirt, Resources.Load<Sprite>("Art/TestDirt")   },
         {   TileType.Grass, Resources.Load<Sprite>("Art/TestGrass")   },
